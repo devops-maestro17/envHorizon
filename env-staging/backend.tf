12f0cd87a8ea2b7048bdc8286eb25c4ba6655ac9
devops-maestro17 <148553140+devops-maestro17@users.noreply.github.com>
2024-01-01 13:27:01 +0000

    adding files

--- env-staging/backend.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "tf-remote-statefile"
-#     key     = "staging/terraform.tfstate"
-#     region  = "ap-south-1"
-#     encrypt = true
-#     # dynamodb_table = "terraform-statelock"
-#   }
-# }
